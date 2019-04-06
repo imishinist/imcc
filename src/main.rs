7ecb86610b97f4295dcb11ae19d013eb88221517
Taisuke Miyazaki <imishinist@gmail.com>
2019-04-06 16:41:07 +0900

    implement strtol

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 use std::env;
+use std::io::{Cursor, SeekFrom, Seek};
+use std::str::FromStr;
+
+
+fn strtol(p: &mut Cursor<&String>) -> Option<i32> {
+    let s = p.get_ref().as_str();
+    let pos = p.position() as usize;
+
+    if let Some(endp) = s.find(|c: char| !c.is_ascii_digit()) {
+        p.set_position(endp as u64);
+        let num_str = &s[pos..endp];
+
+        return FromStr::from_str(num_str).ok();
+    }
+    p.seek(SeekFrom::End(0)).ok();
+    FromStr::from_str(s).ok()
+}
 
 fn main() {
 
@@ -8,9 +25,12 @@ fn main() {
         return;
     }
 
+    let mut p: Cursor<&String> = Cursor::new(args.get(1).unwrap());
+    let num = strtol(&mut p).unwrap();
+
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("  mov rax, {}", args.get(1).unwrap());
+    println!("  mov rax, {}", num);
     println!("  ret");
 }
