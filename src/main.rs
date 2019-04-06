9804cecaf48b293865a34cac49a957cd2750a78c
Taisuke Miyazaki <imishinist@gmail.com>
2019-04-06 22:56:27 +0900

    add parser and generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 use std::io::{Cursor, Read, Seek, SeekFrom};
 use std::str::FromStr;
+use std::rc::Rc;
+use std::error;
+use std::fmt;
+use core::borrow::Borrow;
 
 #[derive(Debug, PartialEq)]
 enum TokenType {
@@ -70,7 +74,12 @@ impl<'a> Tokenizer<'a> {
                 continue;
             }
 
-            if c == '+' || c == '-' {
+            if c == '(' || c == ')' {
+
+            }
+
+            if c == '+' || c == '-' || c == '(' || c == ')'
+             || c == '*' || c == '/' {
                 let token = Token::with_symbol(c);
                 tokens.push(token);
                 continue;
@@ -119,6 +128,157 @@ fn strtol(p: &mut Cursor<&String>) -> Option<i32> {
     FromStr::from_str(s).ok()
 }
 
+#[derive(Debug)]
+enum ParserError {
+    UnexpectedToken(String)
+}
+
+impl fmt::Display for ParserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParserError::UnexpectedToken(err) => write!(f, "Unexpected token: {}", err),
+        }
+    }
+}
+
+impl error::Error for ParserError {
+    fn description(&self) -> &str {
+        match self {
+            ParserError::UnexpectedToken(err) => err.as_str(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Parser {
+    pos: usize,
+    tokens: Vec<Token>,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Self {
+        Parser {
+            pos: 0,
+            tokens: tokens,
+        }
+    }
+
+    fn add(&mut self) -> Result<Node, ParserError> {
+        let mut node = self.mul()?;
+
+        loop {
+            if self.consume(TokenType::Symbol('+')) {
+                node = Node::new_node(TokenType::Symbol('+'), Rc::new(node), Rc::new(self.mul()?));
+            } else if self.consume(TokenType::Symbol('-')) {
+                node = Node::new_node(TokenType::Symbol('-'), Rc::new(node), Rc::new(self.mul()?));
+            } else {
+                return Ok(node);
+            }
+        }
+    }
+
+    fn mul(&mut self) -> Result<Node, ParserError> {
+        let mut node = self.term()?;
+
+        loop {
+            if self.consume(TokenType::Symbol('*')) {
+                node = Node::new_node(TokenType::Symbol('*'), Rc::new(node), Rc::new(self.term()?));
+            } else if self.consume(TokenType::Symbol('/')) {
+                node = Node::new_node(TokenType::Symbol('/'), Rc::new(node), Rc::new(self.term()?));
+            } else {
+                return Ok(node);
+            }
+        }
+    }
+
+    fn term(&mut self) -> Result<Node, ParserError> {
+        if self.consume(TokenType::Symbol('(')) {
+            let node = self.add();
+            if !self.consume(TokenType::Symbol(')')) {
+                let message = self.tokens[self.pos].input.clone();
+                return Err(ParserError::UnexpectedToken(message));
+            }
+            return node;
+        }
+        if self.tokens[self.pos].ty == TokenType::NUM {
+            let pos = self.pos;
+            self.pos += 1;
+            return Ok(Node::new_node_num(self.tokens[pos].val));
+        }
+
+        let message = self.tokens[self.pos].input.clone();
+        Err(ParserError::UnexpectedToken(message))
+    }
+
+    fn consume(&mut self, ty: TokenType) -> bool {
+        if self.tokens[self.pos].ty != ty {
+            return false
+        }
+        self.pos+=1;
+        true
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    ty: TokenType,
+    lhs: Option<Rc<Node>>,
+    rhs: Option<Rc<Node>>,
+    val: i32,
+}
+
+impl Node {
+    fn new_node(ty: TokenType, lhs: Rc<Node>, rhs: Rc<Node>) -> Node {
+        Node {
+            ty: ty,
+            lhs: Some(lhs),
+            rhs: Some(rhs),
+            val: 0,
+        }
+    }
+
+    fn new_node_num(val: i32) -> Node {
+        Node {
+            ty: TokenType::NUM,
+            lhs: None,
+            rhs: None,
+            val: val,
+        }
+    }
+}
+
+fn gen(node: &Node) {
+    if node.ty == TokenType::NUM {
+        println!("  push {}", node.val);
+        return
+    }
+
+    match &node.lhs {
+        None => return,
+        Some(lhs) => gen(lhs.borrow()),
+    }
+
+    match &node.rhs {
+        None => return,
+        Some(rhs) => gen(rhs.borrow()),
+    }
+
+    println!("  pop rdi");
+    println!("  pop rax");
+
+    match node.ty {
+        TokenType::Symbol('+') => { println!("  add rax, rdi"); },
+        TokenType::Symbol('-') => { println!("  sub rax, rdi"); },
+        TokenType::Symbol('*') => { println!("  mul rdi"); },
+        TokenType::Symbol('/') => {
+            println!("  mov rdx, 0");
+            println!("  div rdi");
+        },
+        _ => ()
+    }
+    println!("  push rax");
+}
+
 fn error(token: &Token) {
     eprintln!("予期しないトークンです: {}", token.input);
     std::process::exit(1);
@@ -131,15 +291,24 @@ fn main() {
         return;
     }
 
-    //    let num = strtol(&mut p).unwrap();
-
     let mut tokenizer = Tokenizer::new(args.get(1).unwrap());
     let tokens = tokenizer.tokenize();
+
+    let mut parser = Parser::new(tokens);
+    let ret = parser.add();
+
+    if let Err(e) = ret {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+    let node = ret.unwrap();
 
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
 
+    gen(&node);
+    /*
     if tokens[0].ty != TokenType::NUM {
         eprintln!("error");
         std::process::exit(1);
@@ -175,6 +344,7 @@ fn main() {
         }
         error(&tokens[i]);
     }
-
+    */
+    println!("  pop rax");
     println!("  ret");
 }
